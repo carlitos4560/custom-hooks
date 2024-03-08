743adab013eced2cb754be33ba4f40ad99ae641e
carlitos4560 <carlitosrobertoes@gmail.com>
2024-03-08 15:24:45 -0400

    fix use in useTodo

--- useTodo.js
+++ useTodo.js
@@ -1,6 +1,6 @@
 import { useReducer } from 'react';
 import { useEffect } from 'react';
-import { todoReducer } from '../08-useReduce/todoReducer';
+import { todoReducer } from './todoReducer';
 
 const initialState = [
     {
